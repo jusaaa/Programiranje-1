e4cc95ad7e7111a72770935c5b59a340ddde3762
Jusuf Demic <114824902+jusaaa@users.noreply.github.com>
2022-10-02 16:47:42 +0200

    Update Goldbach.cpp

--- Goldbach.cpp
+++ Goldbach.cpp
@@ -1,23 +1,10 @@
-#include <iostream>;
+/*Svaki paran broj se može prikazati kao suma dva prosta broja (tkz. Goldbachovo pravilo). Napisati program koji će najprije učitati dva prirodna broja n1 i n2. 
+Ako je n1 > n2 zamijeniti n1 sa n2. Zatim prikazati sve parne brojeve u intervalu od n1 do n2 kao sumu dva prosta broja. 
+U glavnom programu samo unijeti navedena dva prirodna broja i pozvati funkciju koja obavlja zadani posao.*/
+
+#include <iostream>
 using namespace std;
-bool prost(int);
-void Goldbach(int, int);
-int main() {
-	int n1, n2,temp;
-	cout << "Unesite vrijednosti n1 i n2: " << endl;
-	cin >> n1 >> n2;
-	if (n1>n2)
-	{
-		
-		temp=n1;
-		n1 = n2;
-	    n2=temp;
-	}
-	Goldbach(n1, n2);
 
-
-	system("pause>0");
-}
 bool prost(int br) {
 	for ( int i = 2; i < br; i++)
 	{
@@ -49,4 +36,21 @@ void Goldbach(int n1, int n2) {
 			}
 		}
 	
+}
+
+int main() {
+	int n1, n2,temp;
+	cout << "Unesite vrijednosti n1 i n2: " << endl;
+	cin >> n1 >> n2;
+	if (n1>n2)
+	{
+		
+		temp=n1;
+		n1 = n2;
+	    n2=temp;
+	}
+	Goldbach(n1, n2);
+
+
+	system("pause>0");
 }
