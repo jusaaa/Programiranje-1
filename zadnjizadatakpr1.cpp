2a3ca5882489f67d7586892d6ab63c3c66192ed7
Jusuf Demic <114824902+jusaaa@users.noreply.github.com>
2022-10-02 17:31:48 +0200

    Add files via upload

---
+++ zadnjizadatakpr1.cpp
@@ -0,0 +1,121 @@
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+struct datum
+{
+	int dan;
+	int mjesec;
+	int godina;
+
+
+
+	void unos()
+	{
+		cout << "Dan: "; cin >> dan;
+		cout << "Mjesec: "; cin >> mjesec;
+		cout << "Godina: "; cin >> godina;
+	}
+
+
+
+	void ispis()
+	{
+		cout << "Dan: " << dan;
+		cout << "Mjesec: " << mjesec;
+		cout << "Godina: " << godina;
+	}
+};
+
+
+
+struct student
+{
+	int IB;
+	char ImeiPrezime[30];
+	datum rodjenja;
+};
+// (*(s)).nesto i s->nesto je isto
+void unosStudenta(student *s)
+{
+
+
+
+	cout << "Unesite broj indeksa: ";
+	cin >> (*(s)).IB;
+	cout << "Ime i prezime: ";
+	cin.ignore();
+	cin.getline((*(s)).ImeiPrezime, 30);
+	cin.ignore();
+	(*(s)).rodjenja.unos();
+}
+
+
+
+void ispisStudenta(student* s)
+{
+	cout << "Broj indeksa: " << s->IB << endl;
+	cout << "Ime i prezime: " << s->ImeiPrezime << endl;
+	cout << "Datum rodjenja: "; s->rodjenja.ispis();
+
+}
+
+
+
+bool validacija(datum* d)
+{
+	if (d->godina > 2022 || d->godina < 1950)
+		return false;
+	if (d->mjesec < 1 || d->mjesec>12)
+		return false;
+	switch (d->mjesec)
+	{
+	case 4:
+	case 8:
+	case 10:
+	case 11: {if (d->dan > 30) return false; }
+	case 2: { if (!((d->godina % 4 == 0 && d->godina % 100 != 0) || d->godina % 400 == 0))
+	{
+		if (d->dan > 28)
+			return false;
+	}
+	}
+	default: if (d->dan > 31) return false;
+	}
+	return true;
+}
+int prebrojavanje(student* s, int vel)
+{
+	int brojac = 0;
+	for (int i = 0; i < vel; i++)
+	{
+		if (!validacija(&s[i].rodjenja))
+			brojac++;
+	}
+	return brojac;
+}
+
+
+
+student* prviUNizu(student* s, int vel)
+{
+	for (int i = 0; i < vel; i++)
+	{
+		if (!validacija(&s[i].rodjenja))
+			return (s + i);
+	}
+	return nullptr;
+}
+int main()
+{
+	int vel = 5;
+	student* nizStudenata=new student[vel];
+	for (size_t i = 0; i < vel; i++)
+	{
+		unosStudenta(nizStudenata + i);
+	}
+	cout << endl;
+	cout<<"Pogresan unos datuma imaju: "<<prebrojavanje(nizStudenata, vel)<<" studenta."<<endl;
+	return 0;
+}
