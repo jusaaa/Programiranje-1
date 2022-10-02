25f9a7ff75c6334e5c320aab73c542aee3a17583
Jusuf Demic <114824902+jusaaa@users.noreply.github.com>
2022-10-02 17:25:40 +0200

    Update and rename rekurzija 1,3,3,9.cpp to Popuni niz rekurzivno.cpp

--- Popuni niz rekurzivno.cpp
+++ Popuni niz rekurzivno.cpp
@@ -1,3 +1,9 @@
+/*Napisati program koji će omogućiti korisniku da unese veličinu n jednodimenzionalnog dinamičkog niza integer vrijednosti (n > 2). 
+Napisati rekurzivnu funkciju koja će taj niz puniti serijom brojeva 1, 3, 3, 9, 27, ..., tako da je svaki slijedeći broj jednak umnosku dva prethodna broja 
+(Prva dva elementa su 1 i 3.) Rekurzija se prekida kad se popuni cijeli niz ili kad se desi overflow na integer varijabli. Zatim napraviti drugu rekurzivnu
+funkciju koja će izračunati zbir svih elemenata tog niza (Voditi računa o tipu podataka zbog veličine broja). U ovom zadatku je zabranjeno indexirati elemente
+niza uglastim zagradama. Obavezno koristiti aritmetiku pokazivača.*/
+
 #include <iostream>
 using namespace std;
 
